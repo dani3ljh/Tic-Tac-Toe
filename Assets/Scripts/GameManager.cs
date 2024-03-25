885102456e406deaaac7f2776e986224b9ef65fa
Daniel Hart <86760228+dani3ljh@users.noreply.github.com>
2024-03-24 22:34:39 -0400

    Added Diagonal Check

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,16 +28,18 @@ public class GameManager : MonoBehaviour
             return;
         isXTurn = !isXTurn;
         turnDisplay.SetTrigger(isXTurn ? "XTurn" : "OTurn");
-        if (CheckRow(num, !isXTurn) || CheckCol(num, !isXTurn))
+        if (
+            CheckRow(num, !isXTurn) ||
+            CheckCol(num, !isXTurn) || 
+            num % 2 == 0 && CheckDiagonals(!isXTurn) // && happens before ||
+        )
             Reset();
     }
 
-    public bool CheckRow(int num, bool isX) {
+    private bool CheckRow(int num, bool isX) {
         int left = num - num % 3;
         int middle = num - num % 3 + 1;
         int right = num - num % 3 + 2;
-
-        print($"checking column {(num - num % 3) / 3} for isX: {isX} at indexes {left}, {middle}, and {right}");
 
         if (cells[left].isX != isX || !cells[left].isFilled)
             return false;
@@ -49,7 +51,7 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
-    public bool CheckCol(int num, bool isX) {
+    private bool CheckCol(int num, bool isX) {
         int top = num % 3;
         int middle = num % 3 + 3;
         int bottom = num % 3 + 6;
@@ -59,6 +61,40 @@ public class GameManager : MonoBehaviour
         if (cells[middle].isX != isX || !cells[middle].isFilled)
             return false;
         if (cells[bottom].isX != isX || !cells[bottom].isFilled)
+            return false;
+        
+        return true;
+    }
+
+    private bool CheckDiagonals(bool isX) {
+        return CheckNegativeDiagonal(isX) || CheckPositiveDiagonal(isX);
+    }
+
+    private bool CheckPositiveDiagonal(bool isX) {
+        int bottomLeft = 6;
+        int middle = 4;
+        int topRight = 2;
+
+        if (cells[bottomLeft].isX != isX || !cells[bottomLeft].isFilled)
+            return false;
+        if (cells[middle].isX != isX || !cells[middle].isFilled)
+            return false;
+        if (cells[topRight].isX != isX || !cells[topRight].isFilled)
+            return false;
+        
+        return true;
+    }
+
+    private bool CheckNegativeDiagonal(bool isX) {
+        int topLeft = 0;
+        int middle = 4;
+        int bottomRight = 8;
+
+        if (cells[topLeft].isX != isX || !cells[topLeft].isFilled)
+            return false;
+        if (cells[middle].isX != isX || !cells[middle].isFilled)
+            return false;
+        if (cells[bottomRight].isX != isX || !cells[bottomRight].isFilled)
             return false;
         
         return true;
